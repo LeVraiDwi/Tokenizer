53f64d5ff47ffbc1131ef5578e31d9c28347845d
LeVraiDwi <67096994+LeVraiDwi@users.noreply.github.com>
2025-06-12 18:11:55 +0200

    Update client.ts

--- client.ts
+++ client.ts
@@ -14,6 +14,7 @@ import {
   createInitializeMintInstruction,
   getMintLen,
   getOrCreateAssociatedTokenAccount,
+  createInitializeMetadataPointerInstruction,
   getMint,
   getMetadataPointerState,
   getTokenMetadata,
@@ -23,43 +24,164 @@ import {
   createMint,
   createMultisig,
 } from "@solana/spl-token";
+import {
+  createInitializeInstruction,
+  createUpdateFieldInstruction,
+  createRemoveKeyInstruction,
+  pack,
+  TokenMetadata,
+} from "@solana/spl-token-metadata";
 
 console.log("log");
 const payer = pg.wallet.keypair;
 const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
 console.log("log");
+// Generate new keypair for Mint Account
+const mintKeypair = Keypair.generate();
+// Address for Mint Account
+const mint = mintKeypair.publicKey;
 
-const ownerOne = Keypair.generate();
-const ownerTwo = Keypair.generate();
-const ownerThree = Keypair.generate();
+const ownerOne = Keypair.fromSecretKey(
+  new Uint8Array([
+    147, 32, 106, 116, 155, 158, 124, 25, 106, 16, 15, 47, 223, 183, 56, 218,
+    90, 68, 58, 190, 163, 54, 221, 136, 237, 202, 141, 50, 221, 53, 6, 112, 72,
+    33, 14, 80, 214, 150, 57, 196, 135, 131, 163, 144, 237, 82, 76, 124, 180,
+    47, 177, 77, 139, 180, 29, 115, 147, 0, 240, 132, 200, 149, 79, 135,
+  ])
+); //5rZbAcF4TQdowbCUTfSAM7MNYQEFudsx3BkiMJYvhTS6
+const ownerTwo = Keypair.fromSecretKey(
+  new Uint8Array([
+    162, 209, 167, 165, 173, 237, 210, 210, 123, 189, 190, 94, 199, 93, 75, 54,
+    130, 99, 217, 225, 205, 89, 143, 66, 178, 115, 177, 139, 250, 183, 193, 63,
+    183, 214, 128, 163, 254, 242, 92, 220, 205, 170, 110, 137, 32, 168, 126, 4,
+    134, 196, 153, 69, 72, 192, 109, 19, 244, 166, 58, 232, 122, 16, 98, 167,
+  ])
+); //DNdKKKaQHVn3Qg9PwdYmqEAGtYpKVYueoiBi64WFvaLn
+const ownerThree = Keypair.fromSecretKey(
+  new Uint8Array([
+    246, 233, 57, 40, 127, 182, 234, 142, 135, 108, 192, 159, 48, 38, 218, 221,
+    123, 62, 30, 75, 107, 159, 223, 33, 229, 73, 95, 91, 55, 216, 187, 200, 210,
+    141, 240, 71, 5, 213, 69, 219, 84, 12, 22, 117, 77, 172, 99, 186, 217, 55,
+    57, 108, 199, 199, 65, 73, 117, 172, 13, 212, 161, 147, 117, 254,
+  ])
+); //FAv9ZyTATm86etFXT6bj2M8G3K6j5C5JmMRTvMGFTTj3
+const multisigne = Keypair.fromSecretKey(
+  new Uint8Array([
+    52, 153, 131, 151, 1, 220, 48, 71, 63, 141, 207, 166, 200, 245, 114, 83,
+    114, 91, 183, 115, 82, 126, 3, 198, 68, 174, 9, 141, 9, 158, 208, 170, 105,
+    52, 38, 4, 126, 134, 157, 34, 6, 67, 57, 133, 232, 88, 136, 183, 204, 98,
+    161, 234, 177, 35, 45, 117, 130, 222, 161, 219, 54, 72, 125, 92,
+  ])
+); //85fvs3HTG51ADso1zEyXuRCxzT2CnWqibNRJMiGHsLrf
 console.log(`ownerOne: ${ownerOne.publicKey} ${ownerOne.secretKey}`);
 console.log(`ownerTwo: ${ownerTwo.publicKey} ${ownerTwo.secretKey}`);
 console.log(`ownerThree: ${ownerThree.publicKey} ${ownerThree.secretKey}`);
 console.log("before multisign");
 
-const multiSign = await createMultisig(
-  connection,
-  payer,
-  [ownerOne.publicKey, ownerTwo.publicKey, ownerThree.publicKey],
-  2,
-  undefined,
-  undefined,
-  TOKEN_2022_PROGRAM_ID
-);
-console.log(`multisigne: ${multiSign}`);
+//const multiSign = await createMultisig(
+//  connection,
+//  payer,
+//  [ownerOne.publicKey, ownerTwo.publicKey, ownerThree.publicKey],
+//  2,
+//  multisigne,
+//  undefined,
+//  TOKEN_2022_PROGRAM_ID
+//);
+console.log(`multisigne: ${multisigne.publicKey} ${multisigne.secretKey}`);
+const multiSign = multisigne.publicKey;
+// Transaction to send
+let transaction: Transaction;
+// Transaction signature returned from sent transaction
+let transactionSignature: string;
 
-const mint = await createMint(
-  connection,
-  payer,
-  multiSign,
-  multiSign,
-  9,
-  undefined,
-  undefined,
-  TOKEN_2022_PROGRAM_ID
+// Metadata to store in Mint Account
+const metaData: TokenMetadata = {
+  updateAuthority: multiSign,
+  mint: mint,
+  name: "42Health",
+  symbol: "FTH",
+  uri: "https://raw.githubusercontent.com/LeVraiDwi/Tokenizer/refs/heads/solana/metadata.json",
+  additionalMetadata: [
+    ["description", "Token that represente amount of data share"],
+  ],
+};
+
+// Size of MetadataExtension 2 bytes for type, 2 bytes for length
+const metadataExtension = TYPE_SIZE + LENGTH_SIZE;
+
+// Size of metadata
+const metadataLen = pack(metaData).length;
+
+// Size of Mint Account with extension
+const mintLen = getMintLen([ExtensionType.MetadataPointer]);
+
+// Minimum lamports required for Mint Account
+const lamports = await connection.getMinimumBalanceForRentExemption(
+  mintLen + metadataExtension + metadataLen
 );
 
 console.log(`mint: ${mint}`);
+
+// Instruction to invoke System Program to create new account
+const createAccountInstruction = SystemProgram.createAccount({
+  fromPubkey: payer.publicKey, // Account that will transfer lamports to created account
+  newAccountPubkey: mint, // Address of the account to create
+  space: mintLen, // Amount of bytes to allocate to the created account
+  lamports, // Amount of lamports transferred to created account
+  programId: TOKEN_2022_PROGRAM_ID, // Program assigned as owner of created account
+});
+
+// Instruction to initialize the MetadataPointer Extension
+const initializeMetadataPointerInstruction =
+  createInitializeMetadataPointerInstruction(
+    mint, // Mint Account address
+    multiSign, // Authority that can set the metadata address
+    mint, // Account address that holds the metadata
+    TOKEN_2022_PROGRAM_ID
+  );
+
+// Instruction to initialize Mint Account data
+const initializeMintInstruction = createInitializeMintInstruction(
+  mint, // Mint Account Address
+  9, // Decimals of Mint
+  multiSign, // Designated Mint Authority
+  null, // Optional Freeze Authority
+  TOKEN_2022_PROGRAM_ID // Token Extension Program ID
+);
+
+// Instruction to initialize Metadata Account data
+const initializeMetadataInstruction = createInitializeInstruction({
+  programId: TOKEN_2022_PROGRAM_ID, // Token Extension Program as Metadata Program
+  metadata: mint, // Account address that holds the metadata
+  updateAuthority: multiSign, // Authority that can update the metadata
+  mint: mint, // Mint Account address
+  mintAuthority: multiSign, // Designated Mint Authority
+  name: metaData.name,
+  symbol: metaData.symbol,
+  uri: metaData.uri,
+});
+
+// Add instructions to new transaction
+transaction = new Transaction().add(
+  createAccountInstruction,
+  initializeMetadataPointerInstruction,
+  // note: the above instructions are required before initializing the mint
+  initializeMintInstruction,
+  initializeMetadataInstruction
+);
+
+// Send transaction
+transactionSignature = await sendAndConfirmTransaction(
+  connection,
+  transaction,
+  [payer, mintKeypair, multisigne] // Signers
+);
+
+console.log(
+  "\nCreate Mint Account:",
+  `https://solana.fm/tx/${transactionSignature}?cluster=devnet-solana`
+);
+
 
 const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
   connection,
@@ -121,7 +243,7 @@ const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
 //
 //console.log(`associeted account: ${associatedTokenAccount}`);
 //
-//try {
+// try {
 //  const rep = await mintTo(
 //    connection,
 //    payer,
@@ -133,11 +255,12 @@ const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
 //    undefined,
 //    TOKEN_2022_PROGRAM_ID
 //  );
-//} catch (e) {
+//  console.log(`${rep}`);
+// } catch (e) {
 //  console.log(e);
-//}
-//
-//try {
+// }
+
+// try {
 //  const rep = await mintTo(
 //    connection,
 //    payer,
@@ -149,6 +272,7 @@ const associatedTokenAccount = await getOrCreateAssociatedTokenAccount(
 //    undefined,
 //    TOKEN_2022_PROGRAM_ID
 //  );
-//} catch (e) {
+//  console.log(`${rep}`);
+// } catch (e) {
 //  console.log(e);
-//}
+// }
